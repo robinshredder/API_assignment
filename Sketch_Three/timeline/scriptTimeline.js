306d8b775643ecb3db9c7d28702e184826c3371e
RobinShredder <robin@schritter.se>
2018-02-22 20:24:25 +0100

    Add files via upload

---
+++ Sketch_Three/timeline/scriptTimeline.js
@@ -0,0 +1,28 @@
+var line = anime({
+    targets: '#relativeOffset .line',
+    translateX: 250,
+    easing: [.90,-0.54,.50,1]
+});
+
+relativeOffset = anime.timeline();
+relativeOffset
+    .add({
+        targets: '#relativeOffset .square.el',
+        translateX: 450,
+        translateY:200,
+        easing: 'easeOutExpo'
+    })
+    .add({
+        targets: '#relativeOffset .circle.el',
+        translateX: 650,
+        translateY: 400,
+        easing: 'easeOutExpo',
+        offset: '-=600' // Starts 600ms before the previous animation ends
+    })
+    .add({
+        targets: '#relativeOffset .triangle.el',
+        translateX: 850,
+        translateY: 200,
+        easing: 'easeOutExpo',
+        offset: '-=800' // Starts 800ms before the previous animation ends
+    });
